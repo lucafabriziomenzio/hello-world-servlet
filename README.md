25fc92fef1934580ea67493ff9c20251dff404cf
kuky54 <luca.fabrizio.menzio@gmail.com>
2024-07-31 18:41:31 +0200

    Project created with NetBeans 8.2, by browsing the URL http://localhost:8084/hello the page crated by the HelloWorldServlet.java say hello to user, any other URL give page 404 Not found

---
+++ README.md
@@ -0,0 +1,8 @@
+Project created with NetBeans 8.2, by browsing the URL 
+
+    http://localhost:8084/hello 
+
+the page crated by the HelloWorldServlet.java say hello to user.
+
+Any other URL give page 404 Not found 
+
---
+++ src/java/com/eng/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package com.eng;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
